c91db574bea01b3bfc4e5eeaf3332b761518afa0
byssup <kin4olda@naver.com>
2017-10-05 11:23:10 +0900

    - 배열 문제(Maximum Swap) type casting(str->int, int->str) 제거

--- array/Maximum Swap.py
+++ array/Maximum Swap.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        nn = [int(n) for n in str(num)]
+        nn = [n for n in str(num)]
         for i, n in enumerate(nn):
             if not nn[i+1:]:
                 continue
 
             max_val = max(nn[i+1:])
             if max_val > n:
-                j = str(num).rfind(str(max_val))
+                j = str(num).rfind(max_val)
                 temp = nn[i]
                 nn[i] = nn[j]
                 nn[j] = temp
-                break
-
-        nn = [str(n) for n in nn]
-        return int(''.join(nn))
+                return int(''.join(nn))
+        return num
